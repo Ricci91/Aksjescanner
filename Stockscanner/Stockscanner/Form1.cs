47de94859c27ec4c942f3aa286b75d4ff3f5f33c
Ricci91 <froyland.richard@gmail.com>
2020-05-18 15:54:12 +0200

    first commit

---
+++ Stockscanner/Stockscanner/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stockscanner
+{
+    public partial class Form1 : Form
+    {
+        Scanner scanner;
+        public Form1()
+        {
+            InitializeComponent();
+            scanner = new Scanner();
+        }
+
+        private void scanButton_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show(scanner.scan());
+        }
+    }
+}
